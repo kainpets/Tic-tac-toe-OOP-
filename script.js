4dc50d836219281d0678b306c1bc220a5a47bb8c
kainpets <pawelstepniak22@gmail.com>
2022-06-05 23:10:54 +0200

    created grid and grid items

---
+++ script.js
@@ -0,0 +1,21 @@
+class Gameboard {
+  constructor() {
+
+  }
+}
+
+class Players() {
+  constructor() {
+
+  }
+}
+
+class gameFlow() {
+  constructor() {
+
+  }
+}
+
+function renderBoard() {
+   
+}
